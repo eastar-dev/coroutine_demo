04d8495185764e6b3aa5ecf5fecf815bfd566b22
pluto <pluto@eastandroid.com>
2019-03-26 10:53:47 +0900

    no message

--- app/src/main/java/dev/eastar/coroutine/demo/IntroAfter.kt
+++ app/src/main/java/dev/eastar/coroutine/demo/IntroAfter.kt
@@ -9,95 +9,88 @@ import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.common.internal.ConnectionErrorMessages.getAppName
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
-import kotlin.coroutines.coroutineContext
+import kotlinx.coroutines.*
+import kotlin.coroutines.resume
 
 class IntroAfter : AppCompatActivity() {
-    private var rootingChecked: Boolean = false
-    private var v3Checked: Boolean = false
-    private var gcmChecked: Boolean = false
-    private var bannerChecked: Boolean = false
-    private var v3UpdateChecked: Boolean = false
-    private var noticeChecked: Boolean = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(Button(this).apply {
             setOnClickListener { load() }
             text = "start intro after"
         })
+        load()
+    }
+
+    //test stateus
+    companion object {
+        val IS_ROOTING = false
+        val NOT_INSTALLED_V3 = false
+        val HAS_EVENT = false
     }
 
     fun load() = CoroutineScope(Dispatchers.Main).launch {
-        Log.e()
-        toast("check rootingChecked : $rootingChecked")
+        Log.e(0)
+        toast("rootingChecked : start")
         step00RootingCheck()
-        Log.e()
-        toast("check v3Checked : $v3Checked")
+        toast("rootingChecked : end")
+        Log.e(1)
+        toast("step01v3Check : start")
         step01v3Check()
-        Log.e()
-        toast("check gcmChecked : $gcmChecked")
-        step04gcmCheck()
-        Log.e()
-        toast("check v3UpdateChecked : $v3UpdateChecked")
-        step07v3UpdateCheck()
-        Log.e()
-        toast("check noticeChecked : $noticeChecked")
-        step11noticeCheck()
-        Log.e()
-        step12checkSSO()
-        Log.e()
-    }
-//    fun load() = CoroutineScope(Dispatchers.Main).launch {
-//
-//        launch {
-//            Log.e()
-//            toast("check rootingChecked : $rootingChecked")
-//            step00RootingCheck()
-//            Log.e()
-//        }.join()
-//        launch {
-//            toast("check v3Checked : $v3Checked")
-//            step01v3Check()
-//            Log.e()
-//        }.join()
-//        launch {
-//            toast("check gcmChecked : $gcmChecked")
-//            step04gcmCheck()
-//            Log.e()
-//        }.join()
-//        launch {
-//            toast("check v3UpdateChecked : $v3UpdateChecked")
-//            step07v3UpdateCheck()
-//            Log.e()
-//        }.join()
-//        launch {
-//            toast("check noticeChecked : $noticeChecked")
-//            step11noticeCheck()
-//            Log.e()
-//        }.join()
-//        launch {
-//            step12checkSSO()
-//            Log.e()
-//        }.join()
-//    }
-
-    suspend private fun step00RootingCheck() {
-        if (NOT_ROOTING != isRooting()) {// 루팅 또는 기타오류
-            val message = "루팅된 단말입니다. 개인정보 유출의 위험성이 있으므로 " + getAppName(this) + "를 종료합니다."
-            showDialogForceExit(message)
+        toast("step01v3Check : end")
+        Log.e(2)
+        val deferredGcm = async {
+            toast("step04gcmCheck : start")
+            step04gcmCheck()
+            toast("step04gcmCheck : end")
+            Log.e(3)
         }
-        Log.e()
-        coroutineContext.cancel()
-        Log.e()
+        val deferredBanner = async {
+            toast("step06assetBannerCheck : start")
+            step06assetBannerCheck()
+            toast("step06assetBannerCheck : end")
+            Log.e(4)
+        }
+        Log.e(5)
+        toast("step07v3UpdateCheck : start")
+        step07v3UpdateCheck()
+        toast("step07v3UpdateCheck : end")
+        Log.e(6)
+        toast("step11noticeCheck : start")
+        step11noticeCheck()
+        toast("step11noticeCheck : end")
+        Log.e(7)
+        toast("step12checkSSO : start")
+        step12checkSSO()
+        toast("step12checkSSO : end")
+        Log.e(8)
+        toast("await : start")
+        deferredGcm.await()
+        deferredBanner.await()
+        toast("await : end")
+        Log.e(99)
     }
 
-    private suspend fun step01v3Check() {
+    private suspend fun step00RootingCheck() = suspendCancellableCoroutine<Unit> { continuation ->
+        if (IS_ROOTING) {// 루팅 또는 기타오류
+            val message = "루팅된 단말입니다. 개인정보 유출의 위험성이 있으므로 " + getAppName(this@IntroAfter) + "를 종료합니다."
+            AlertDialog.Builder(this).setMessage(message)
+                    .setPositiveButton("종료", null)
+                    .setCancelable(false)
+                    .setOnDismissListener {
+                        continuation.cancel()
+                        exit()
+                    }
+                    .show()
+            return@suspendCancellableCoroutine
+        }
+        continuation.resume(Unit)
+    }
+
+    private suspend fun step01v3Check() = suspendCancellableCoroutine<Unit> { continuation ->
         executeV3Module(object : V3MobilePlusResultListener {
             override fun OnInstallCompleted() {
+                continuation.resume(Unit)
             }
 
             override fun OnPatchState(arg0: Boolean) {}
@@ -105,49 +98,59 @@ class IntroAfter : AppCompatActivity() {
         })
     }
 
-    suspend private fun step04gcmCheck() {
-        gcmRegister(applicationContext, GCM_SENDER_ID)
+    private fun step04gcmCheck() {
+        gcmRegister(applicationContext, "GCM_SENDER_ID")
     }
 
-    suspend private fun step06assetBannerCheck() {
+    private fun step06assetBannerCheck() {
+        //do banner work
     }
 
-    suspend private fun step07v3UpdateCheck() {
-        //doV3Update
-        AlertDialog.Builder(this).setMessage("V3 설치안됨")
-                .setPositiveButton("설치하기") { _, _ ->
-                    val intent = Intent(Intent.ACTION_VIEW)
-                    intent.data = Uri.parse("market://details?id=com.ahnlab.v3mobileplus")
-                    startActivity(intent)
-                    exit()
-                }
-                .setNegativeButton("계속진행") { _, _ ->
-                }
-                .show()
+    private suspend fun step07v3UpdateCheck() = suspendCancellableCoroutine<Unit> { continuation ->
+        if (NOT_INSTALLED_V3) {
+            AlertDialog.Builder(this).setMessage("V3 설치안됨")
+                    .setPositiveButton("설치하기") { _, _ ->
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.data = Uri.parse("market://details?id=com.ahnlab.v3mobileplus")
+                        startActivity(intent)
+                        continuation.cancel()
+                    }
+                    .setNegativeButton("종료") { _, _ ->
+                        continuation.cancel()
+                    }
+                    .show()
+            return@suspendCancellableCoroutine
+        }
+        continuation.resume(Unit)
     }
 
-    suspend private fun step11noticeCheck() {
+    private suspend fun step11noticeCheck() = suspendCancellableCoroutine<Unit> { continuation ->
+        try {
+            if (HAS_EVENT) {
+                AlertDialog.Builder(this)
+                        .setMessage("신규 고객 이벤트")
+                        .setPositiveButton("참여하기") { _, _ ->
+                            startActivity(Intent("android.intent.action.EVENT_VIEW"))
+                            continuation.cancel()
+                        }
+                        .setNegativeButton("나중에보기") { _, _ ->
+                            continuation.resume(Unit)
+                        }
+                        .show()
+                return@suspendCancellableCoroutine
+            }
+        } finally {
+            continuation.resume(Unit)
+        }
     }
 
-    suspend private fun step12checkSSO() {
+    private fun step12checkSSO() {
+        main()
     }
 
     //------------------------------------------------------------------------------------------
     private fun executeV3Module(v3MobilePlusResultListener: V3MobilePlusResultListener?) {
         v3MobilePlusResultListener?.OnInstallCompleted()
-    }
-
-    companion object {
-        val NOT_ROOTING: Int = 0
-        val GCM_SENDER_ID: Any? = null
-    }
-
-    private fun isRooting(): Int {
-        return NOT_ROOTING
-    }
-
-    private fun showDialogForceExit(message: String) {
-        finish()
     }
 
     private fun gcmRegister(applicationContext: Context?, gcM_SENDER_ID: Any?) {
@@ -159,11 +162,13 @@ class IntroAfter : AppCompatActivity() {
     }
 
     private fun exit() {
-        finish()
+//        finish()
+        toast("종료됨")
+        Log.e("종료됨")
     }
 
     private fun main() {
-        finish()
+//        finish()
     }
 
     interface V3MobilePlusResultListener {
--- app/src/test/java/dev/eastar/coroutine/demo/ExampleUnitTest.kt
+++ app/src/test/java/dev/eastar/coroutine/demo/ExampleUnitTest.kt
@@ -1,8 +1,13 @@
 package dev.eastar.coroutine.demo
 
+import android.log.Log
+import io.reactivex.Observable
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +19,47 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun concatTest() {
+        Log.MODE = Log.eMODE.SYSTEMOUT
+        assertEquals("ABCDEredyellowblue", block())
+    }
+}
+
+fun block(): String? {
+    Log.e(1)
+//    var result: T? = null
+    var sss: String? = null
+    runBlocking {
+        Log.e(2)
+        sss = suspendCoroutine<String> { continuation ->
+            Log.e(3)
+            val resultString = StringBuilder()
+            val shapes = listOf("A", "B", "C", "D", "E")
+            val colorList = listOf("red", "yellow", "blue")
+            val source: Observable<String> = Observable.concat(
+                    Observable.interval(0L, 30L, TimeUnit.MILLISECONDS)
+                            .map { index -> shapes[index.toInt()] }
+                            .take(shapes.size.toLong()),
+                    Observable.interval(0L, 100L, TimeUnit.MILLISECONDS)
+                            .map { index -> colorList[index.toInt()] }
+                            .take(colorList.size.toLong())
+            )
+            source.subscribe({
+                Log.e(4)
+                resultString.append(it)
+                println("${Thread.currentThread()} $it")
+            }, {
+                Log.e(5)
+            }, {
+                Log.e(6)
+                continuation.resume(resultString.toString())
+            })
+        }
+        Log.e(7)
+    }
+    Log.e(8)
+//    return result
+    return sss
 }
